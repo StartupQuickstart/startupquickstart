43f16fed1bb976fe3a560eeb2f6255b31f9f8f42
Thomas Boles <tcboles@me.com>
2022-01-08 22:31:10 -0500

    Fixes font imports in css files

--- packages/react/build.sh
+++ packages/react/build.sh
@@ -10,3 +10,4 @@ cp -r ./src/assets/fonts dist/assets/fonts
 cp -r ./src/assets/images dist/assets/images
 
 node-sass ./src/assets/scss/main.scss -o dist/assets/css
+cd ./dist/assets/css/ && sed -i '' 's|../../fonts|../fonts|g' main.css
