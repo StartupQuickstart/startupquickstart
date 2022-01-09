4966cca55553417a65110fb61b64614d73720c63
Thomas Boles <tcboles@me.com>
2022-01-08 21:27:30 -0500

    Fixes build script

--- packages/react/build.sh
+++ packages/react/build.sh
@@ -4,3 +4,9 @@ babel ./src/components -d dist/components --copy-files
 babel ./src/context -d dist/context --copy-files
 babel ./src/lib -d dist/lib --copy-files
 babel ./src/views -d dist/views --copy-files
+
+mkdir ./dist/assets
+cp -r ./src/assets/fonts dist/assets/fonts
+cp -r ./src/assets/images dist/assets/images
+
+node-sass ./src/assets/scss/main.scss -o dist/assets/css
