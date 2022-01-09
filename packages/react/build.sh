0bc301f7495cb098190fd8e5ee39cab3fdeda0e3
Thomas Boles <tcboles@me.com>
2022-01-08 21:04:37 -0500

    Updates build script

--- packages/react/build.sh
+++ packages/react/build.sh
@@ -1,6 +1,6 @@
 rm -rf dist
 mkdir dist
-babel ./src/components -d dist --copy-files
-babel ./src/context -d dist --copy-files
-babel ./src/lib -d dist --copy-files
-babel ./src/views -d dist --copy-files
+babel ./src/components -d dist/components --copy-files
+babel ./src/context -d dist/context --copy-files
+babel ./src/lib -d dist/lib --copy-files
+babel ./src/views -d dist/views --copy-files
