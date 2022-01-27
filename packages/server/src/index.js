fa52f407402b08ec9d713d4fa54649be13f25fb0
Thomas Boles <tcboles@me.com>
2022-01-26 22:49:12 -0500

    fixes build script

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -1,6 +1,6 @@
 export * as api from '@/api';
 export * as lib from '@/lib';
 export * as passports from '@/passports';
-export server from './server';
+export * as server from './server';
 
 export default { api, lib, server };
